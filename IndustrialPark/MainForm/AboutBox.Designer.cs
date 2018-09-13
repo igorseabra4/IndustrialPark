30cfb7d37ecdda91cab2a79e40b8017ef0cef394
igorseabra4 <igorseabra@gmail.com>
2018-09-13 00:02:46 -0300

    preview 5

--- IndustrialPark/MainForm/AboutBox.Designer.cs
+++ IndustrialPark/MainForm/AboutBox.Designer.cs
@@ -102,7 +102,7 @@
             this.labelVersion.Name = "labelVersion";
             this.labelVersion.Size = new System.Drawing.Size(271, 17);
             this.labelVersion.TabIndex = 0;
-            this.labelVersion.Text = "Preview 4";
+            this.labelVersion.Text = "Preview 5";
             this.labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // labelCopyright
