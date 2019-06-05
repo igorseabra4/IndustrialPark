4a4f53fd4906513fd06e4c25b3c5f749d3fad970
igorseabra4 <igor.seabra@ccc.ufcg.edu.br>
2019-06-05 02:31:50 -0300

    organize code

--- IndustrialPark/MainForm/MainForm.cs
+++ IndustrialPark/MainForm/MainForm.cs
@@ -7,11 +7,6 @@ using System.IO;
 using System.Linq;
 using System.Windows.Forms;
 using Newtonsoft.Json;
-using System.IO.Compression;
-using System.Web;
-using System.Net;
-using System.Threading.Tasks;
-using System.ComponentModel;
 
 namespace IndustrialPark
 {
@@ -51,7 +46,7 @@ namespace IndustrialPark
                 autoLoadOnStartupToolStripMenuItem.Checked = settings.AutoloadOnStartup;
                 checkForUpdatesOnStartupToolStripMenuItem.Checked = settings.CheckForUpdatesOnStartup;
 
-                if (settings.CheckForUpdatesOnStartup && UpdateIndustrialPark(out _))
+                if (settings.CheckForUpdatesOnStartup && AutomaticUpdater.UpdateIndustrialPark(out _))
                 {
                     Close();
                     System.Diagnostics.Process.Start(Application.StartupPath + "\\IndustrialPark.exe");
@@ -218,7 +213,7 @@ namespace IndustrialPark
 
         private void CheckForUpdatesNowToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (UpdateIndustrialPark(out bool hasChecked))
+            if (AutomaticUpdater.UpdateIndustrialPark(out bool hasChecked))
             {
                 Close();
                 System.Diagnostics.Process.Start(Application.StartupPath + "\\IndustrialPark.exe");
@@ -227,127 +222,9 @@ namespace IndustrialPark
                 MessageBox.Show("No update found.");
         }
 
-        public bool UpdateIndustrialPark(out bool hasChecked)
-        {
-            hasChecked = false;
-
-            try
-            {
-                string versionInfoURL = "https://raw.githubusercontent.com/igorseabra4/IndustrialPark/master/IndustrialPark/Resources/ip_version.json";
-
-                string updatedJson;
-
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(versionInfoURL);
-                request.AutomaticDecompression = DecompressionMethods.GZip;
-                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
-                    updatedJson = reader.ReadToEnd();
-                
-                IPversion updatedVersion = JsonConvert.DeserializeObject<IPversion>(updatedJson);
-                IPversion oldVersion = new IPversion();
-
-                hasChecked = true;
-
-                if (oldVersion.version != updatedVersion.version)
-                {
-                    string messageText = "There is an update available for Industrial Park: " + updatedVersion.versionName + ". Do you wish to download it?";
-                    DialogResult d = MessageBox.Show(messageText, "Update Available", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
-
-                    if (d == DialogResult.Yes)
-                    {
-                        string updatedIPfileName = "IndustrialPark_" + updatedVersion.version.Replace('p', 'P') + ".zip";
-                        string updatedIPURL = "https://github.com/igorseabra4/IndustrialPark/releases/download/" + updatedVersion.version + "/" + updatedIPfileName;
-
-                        string updatedIPfilePath = Application.StartupPath + "\\Resources\\" + updatedIPfileName;
-
-                        using (var webClient = new WebClient())
-                            webClient.DownloadFile(updatedIPURL, updatedIPfilePath);
-
-                        string oldPath = Application.StartupPath + "\\IndustrialPark_old\\";
-
-                        if (!Directory.Exists(oldPath))
-                            Directory.CreateDirectory(oldPath);
-
-                        foreach (string s in new string[]
-                        {
-                                "",
-                                "\\Resources",
-                                "\\Resources\\importvcolorobj",
-                                "\\Resources\\Models",
-                                "\\Resources\\SharpDX"
-                        })
-                        {
-                            if (!Directory.Exists(oldPath + s))
-                                Directory.CreateDirectory(oldPath + s);
-
-                            foreach (string s2 in Directory.GetFiles(Application.StartupPath + s))
-                            {
-                                if (Path.GetExtension(s2).ToLower().Equals(".zip") || Path.GetExtension(s2).ToLower().Equals(".json"))
-                                    continue;
-
-                                string newFilePath = oldPath + s + "\\" + Path.GetFileName(s2);
-
-                                if (File.Exists(newFilePath))
-                                    File.Delete(newFilePath);
-
-                                File.Move(s2, newFilePath);
-                            }
-                        }
-                        
-                        ZipFile.ExtractToDirectory(updatedIPfilePath, Application.StartupPath);
-
-                        File.Delete(updatedIPfilePath);
-
-                        return true;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("There was an error checking for updates: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-
-            return false;
-        }
-
         private void DownloadIndustrialParkEditorFilesToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            try
-            {
-                string editorFilesZipURL = "https://github.com/igorseabra4/IndustrialPark-EditorFiles/archive/master.zip";
-                string destZipPath = Application.StartupPath + "\\Resources\\IndustrialPark-EditorFiles.zip";
-                string editorFilesFolder = Application.StartupPath + "\\Resources\\IndustrialPark-EditorFiles\\";
-
-                MessageBox.Show("Will begin download of IndustrialPark-EditorFiles from GitHub to " + editorFilesFolder + ". Please wait as this might take a while. Any previously existing files in the folder will be overwritten.");
-
-                using (var webClient = new WebClient())
-                    webClient.DownloadFile(new Uri(editorFilesZipURL), destZipPath);
-
-                RecursiveDelete(editorFilesFolder);
-
-                ZipFile.ExtractToDirectory(destZipPath, editorFilesFolder);
-
-                File.Delete(destZipPath);
-
-                MessageBox.Show("Downloaded newest IndustrialPark-EditorFiles to " + editorFilesFolder);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-        private static void RecursiveDelete(string directory)
-        {
-            if (!Directory.Exists(directory))
-                return;
-
-            foreach (var dir in Directory.GetDirectories(directory))
-                RecursiveDelete(dir);
-
-            foreach (var s in Directory.GetFiles(directory))
-                File.Delete(s);
-
-            Directory.Delete(directory);
+            AutomaticUpdater.DownloadEditorFiles();
         }
 
         public ProjectJson FromCurrentInstance()
---
+++ IndustrialPark/Other/AutomaticUpdater.cs
@@ -0,0 +1,136 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Windows.Forms;
+
+namespace IndustrialPark
+{
+    public static class AutomaticUpdater
+    {
+        public static bool UpdateIndustrialPark(out bool hasChecked)
+        {
+            hasChecked = false;
+
+            try
+            {
+                string versionInfoURL = "https://raw.githubusercontent.com/igorseabra4/IndustrialPark/master/IndustrialPark/Resources/ip_version.json";
+
+                string updatedJson;
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(versionInfoURL);
+                request.AutomaticDecompression = DecompressionMethods.GZip;
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    updatedJson = reader.ReadToEnd();
+
+                IPversion updatedVersion = JsonConvert.DeserializeObject<IPversion>(updatedJson);
+                IPversion oldVersion = new IPversion();
+
+                hasChecked = true;
+
+                if (oldVersion.version != updatedVersion.version)
+                {
+                    string messageText = "There is an update available for Industrial Park: " + updatedVersion.versionName + ". Do you wish to download it?";
+                    DialogResult d = MessageBox.Show(messageText, "Update Available", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+                    if (d == DialogResult.Yes)
+                    {
+                        string updatedIPfileName = "IndustrialPark_" + updatedVersion.version.Replace('p', 'P') + ".zip";
+                        string updatedIPURL = "https://github.com/igorseabra4/IndustrialPark/releases/download/" + updatedVersion.version + "/" + updatedIPfileName;
+
+                        string updatedIPfilePath = Application.StartupPath + "\\Resources\\" + updatedIPfileName;
+
+                        using (var webClient = new WebClient())
+                            webClient.DownloadFile(updatedIPURL, updatedIPfilePath);
+
+                        string oldPath = Application.StartupPath + "\\IndustrialPark_old\\";
+
+                        if (!Directory.Exists(oldPath))
+                            Directory.CreateDirectory(oldPath);
+
+                        foreach (string s in new string[]
+                        {
+                                "",
+                                "\\Resources",
+                                "\\Resources\\importvcolorobj",
+                                "\\Resources\\Models",
+                                "\\Resources\\SharpDX"
+                        })
+                        {
+                            if (!Directory.Exists(oldPath + s))
+                                Directory.CreateDirectory(oldPath + s);
+
+                            foreach (string s2 in Directory.GetFiles(Application.StartupPath + s))
+                            {
+                                if (Path.GetExtension(s2).ToLower().Equals(".zip") || Path.GetExtension(s2).ToLower().Equals(".json"))
+                                    continue;
+
+                                string newFilePath = oldPath + s + "\\" + Path.GetFileName(s2);
+
+                                if (File.Exists(newFilePath))
+                                    File.Delete(newFilePath);
+
+                                File.Move(s2, newFilePath);
+                            }
+                        }
+
+                        ZipFile.ExtractToDirectory(updatedIPfilePath, Application.StartupPath);
+
+                        File.Delete(updatedIPfilePath);
+
+                        return true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("There was an error checking for updates: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            return false;
+        }
+
+        public static void DownloadEditorFiles()
+        {
+            try
+            {
+                string editorFilesZipURL = "https://github.com/igorseabra4/IndustrialPark-EditorFiles/archive/master.zip";
+                string destZipPath = Application.StartupPath + "\\Resources\\IndustrialPark-EditorFiles.zip";
+                string editorFilesFolder = Application.StartupPath + "\\Resources\\IndustrialPark-EditorFiles\\";
+
+                MessageBox.Show("Will begin download of IndustrialPark-EditorFiles from GitHub to " + editorFilesFolder + ". Please wait as this might take a while. Any previously existing files in the folder will be overwritten.");
+
+                using (var webClient = new WebClient())
+                    webClient.DownloadFile(new Uri(editorFilesZipURL), destZipPath);
+
+                RecursiveDelete(editorFilesFolder);
+
+                ZipFile.ExtractToDirectory(destZipPath, editorFilesFolder);
+
+                File.Delete(destZipPath);
+
+                MessageBox.Show("Downloaded IndustrialPark-EditorFiles from https://github.com/igorseabra4/IndustrialPark-EditorFiles/ to " + editorFilesFolder);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private static void RecursiveDelete(string directory)
+        {
+            if (!Directory.Exists(directory))
+                return;
+
+            foreach (var dir in Directory.GetDirectories(directory))
+                RecursiveDelete(dir);
+
+            foreach (var s in Directory.GetFiles(directory))
+                File.Delete(s);
+
+            Directory.Delete(directory);
+        }
+    }
+}
