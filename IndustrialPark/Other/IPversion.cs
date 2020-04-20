33f91f28d4f1a2879581ae70ad017a4b97485eae
igorseabra4 <igor.seabra@ccc.ufcg.edu.br>
2020-04-20 02:22:16 -0300

    preview 59.1

--- IndustrialPark/Other/IPversion.cs
+++ IndustrialPark/Other/IPversion.cs
@@ -2,7 +2,7 @@
 {
     public class IPversion
     {
-        public string version = "preview59";
-        public string versionName = "Preview 59";
+        public string version = "preview59.1";
+        public string versionName = "Preview 59.1";
     }
 }
