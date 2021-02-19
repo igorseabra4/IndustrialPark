423e0d02af96610eecaa2e6ec15bef03a1760a01
igorseabra4 <igor.seabra@ccc.ufcg.edu.br>
2021-02-19 01:13:06 -0300

    add movie conditionals

--- IndustrialPark/Assets/ObjectAssets/AssetCOND.cs
+++ IndustrialPark/Assets/ObjectAssets/AssetCOND.cs
@@ -14,7 +14,7 @@ namespace IndustrialPark
         NOT_EQUAL_TO = 5
     }
 
-    public enum CONDVariable : uint
+    public enum CONDVariable_BFBB : uint
     {
         SoundMode = 0x29600EB0,
         MusicVolume = 0x84D4A26D,
@@ -34,6 +34,34 @@ namespace IndustrialPark
         CounterValue = 0x4329EFFD,
         IsEnabled = 0xA6956B3F,
         IsVisible = 0x1E42996C
+    }
+
+    public enum CONDVariable_TSSM : uint
+    {
+        SoundMode = 0x29600EB0,
+        MusicVolume = 0x84D4A26D,
+        SFXVolume = 0x1E0EEB55,
+        MemoryCardAvailable = 0x42453758,
+        IsVibrationOn = 0x3B93C93F,
+        AreSubtitlesEnabled = 0xD1A7DE2C,
+        LetterOfScene = 0x704D04A9,
+        Room = 0x0B11B427,
+        CurrentDate = 0x9482683D,
+        CurrentHour = 0x950F49B7,
+        CurrentMinute = 0xBD2884E7,
+        CounterValue = 0x4329EFFD,
+        IsEnabled = 0xA6956B3F,
+        IsVisible = 0x1E42996C,
+        TimerSecondsLeft = 0x6897B48B,
+        TimerMillisecondsLeft = 0xF4FE2282,
+        IsMNUS = 0x649FA12A,
+        DemoType = 0x0B9F22CF,
+        GoofyGooberTokens = 0x43DD1E00,
+        ManlinessPoints = 0xD8A29291,
+        LevelTreasureChests = 0xFE31C583,
+        PlayerCurrentHealth = 0x25CD9F4A,
+        IsReferenceNULL = 0x1F5BAA4D,
+        AlwaysPortal = 0x5B85F809
     }
 
     public class AssetCOND : BaseAsset
@@ -58,21 +86,33 @@ namespace IndustrialPark
         {
             if (game == Game.Scooby)
             {
-                dt.RemoveProperty("Conditional");
+                dt.RemoveProperty("Conditional_BFBB");
                 dt.RemoveProperty("AssetUnderEvaluation");
             }
             else
                 dt.RemoveProperty("ScoobyConditional");
+
+            if (game == Game.Incredibles)
+                dt.RemoveProperty("Conditional_BFBB");
+            else
+                dt.RemoveProperty("Conditional_TSSM");
 
             base.SetDynamicProperties(dt);
         }
 
         protected override int EventStartOffset => game == Game.Scooby ? 0x14 : 0x18;
 
-        [Category("Conditional")]
-        public CONDVariable Conditional
+        [Category("Conditional"), DisplayName("Conditional")]
+        public CONDVariable_BFBB Conditional_BFBB
         {
-            get => (CONDVariable)ReadUInt(0xC);
+            get => (CONDVariable_BFBB)ReadUInt(0xC);
+            set => Write(0xC, (uint)value);
+        }
+
+        [Category("Conditional"), DisplayName("Conditional")]
+        public CONDVariable_TSSM Conditional_TSSM
+        {
+            get => (CONDVariable_TSSM)ReadUInt(0xC);
             set => Write(0xC, (uint)value);
         }
 
