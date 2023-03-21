26b2043cbf6367eb965abdeb9d14c100a19b5942
igorseabra4 <igor.seabra@ccc.ufcg.edu.br>
2023-03-21 09:59:42 -0300

    rename file

--- IndustrialPark/Assets/Binary/AssetSNDI_GCN_V2.cs
+++ IndustrialPark/Assets/Binary/AssetSNDI_GCN_V2.cs
@@ -1,297 +1,212 @@
-﻿//using HipHopFile;
-//using System;
-//using System.Collections.Generic;
-//using System.ComponentModel;
-//using System.IO;
-//using System.Linq;
-
-//namespace IndustrialPark
-//{
-//    public class AssetSNDI_GCN_V2 : Asset
-//    {
-//        public override string AssetInfo => $"GameCube {game}, {Entries.Sum(e => e.soundEntries.Length)} entries";
-
-//        private byte ReadByte(int j)
-//        {
-//            return Data[j];
-//        }
-
-//        private short ReadShort(int j)
-//        {
-//            return BitConverter.ToInt16(new byte[] {
-//                Data[j + 1],
-//                Data[j] }, 0);
-//        }
-
-//        private int ReadInt(int j)
-//        {
-//            return BitConverter.ToInt32(new byte[] {
-//                Data[j + 3],
-//                Data[j + 2],
-//                Data[j + 1],
-//                Data[j] }, 0);
-//        }
-
-//        private void Write(int j, byte value)
-//        {
-//            Data[j] = value;
-//        }
-
-//        private void Write(int j, short value)
-//        {
-//            byte[] split = BitConverter.GetBytes(value).Reverse().ToArray();
-
-//            for (int i = 0; i < 2; i++)
-//                Data[j + i] = split[i];
-//        }
-
-//        private void Write(int j, int value)
-//        {
-//            byte[] split = BitConverter.GetBytes(value).Reverse().ToArray();
-
-//            for (int i = 0; i < 4; i++)
-//                Data[j + i] = split[i];
-//        }
-
-//        private Section_AHDR AHDR;
-//        private byte[] Data
-//        {
-//            get => AHDR.data;
-//            set => AHDR.data = value;
-//        }
-
-//        // most of this file is a very hacky temporary thing
-
-//        public AssetSNDI_GCN_V2(string assetName) : base(assetName, AssetType.SoundInfo)
-//        {
-//            AHDR = new Section_AHDR(assetID, assetType, flags, new Section_ADBG(0, assetName, assetFileName, checksum), new byte[0x20]);
-//        }
-
-//        public AssetSNDI_GCN_V2(Section_AHDR AHDR, Game game, Endianness endianness) : base(AHDR, game)
-//        {
-//            this.AHDR = AHDR;
-//        }
-
-//        public override void Serialize(EndianBinaryWriter writer)
-//        {
-//            writer.Write(AHDR.data);
-//        }
-
-//        private int FooterOffset
-//        {
-//            get => ReadInt(0x04) + 0x20;
-//            set => Write(0x04, value - 0x20);
-//        }
-
-//        private short TotalSoundCount
-//        {
-//            get => ReadShort(0x18);
-//            set => Write(0x18, value);
-//        }
-
-//        private short SoundCountFirstFile
-//        {
-//            get => ReadShort(0x1A);
-//            set => Write(0x1A, value);
-//        }
-
-//        private short SoundCountRest
-//        {
-//            set => Write(0x1C, value);
-//        }
-
-//        private byte FileCount
-//        {
-//            get => ReadByte(0x1E);
-//            set => Write(0x1E, value);
-//        }
-
-//        private byte UnknownCount
-//        {
-//            set => Write(0x1F, value);
-//        }
-
-//        private int EOF => FooterOffset + 4 * FileCount + 8 * TotalSoundCount;
-
-//        [Category("Sound Info")]
-//        public FSB3_File[] Entries
-//        {
-//            get => DeserializeAsset(Data, this);
-//            set => Data = SerializeData(value, AHDR);
-//        }
-
-//        private uint Switch(uint a) => BitConverter.ToUInt32(BitConverter.GetBytes(a).Reverse().ToArray(), 0);
-
-//        private static FSB3_File[] DeserializeAsset(byte[] data, AssetSNDI_GCN_V2 asset)
-//        {
-//            BinaryReader binaryReader = new BinaryReader(new MemoryStream(data));
-
-//            List<FSB3_File> entries = new List<FSB3_File>();
-
-//            for (int i = 0; i < asset.FileCount; i++)
-//            {
-//                binaryReader.BaseStream.Position = asset.FooterOffset + 4 * i;
-//                binaryReader.BaseStream.Position = asset.Switch(binaryReader.ReadUInt32()) + 0x20;
-
-//                entries.Add(new FSB3_File(binaryReader));
-//            }
-
-//            binaryReader.BaseStream.Position = asset.FooterOffset + 4 * asset.FileCount;
-
-//            for (int i = 0; i < asset.TotalSoundCount; i++)
-//            {
-//                GcWavInfo a = new GcWavInfo();
-//                a.SetEntryPartTwo(binaryReader);
-//                entries[a.uFSBIndex].soundEntries[a.uAudioSampleIndex].SetEntryPartTwo(a);
-//            }
-
-//            return entries.ToArray();
-//        }
-
-//        private static byte[] SerializeData(FSB3_File[] value, Section_AHDR AHDR)
-//        {
-//            List<byte> newData = new List<byte>();
-//            newData.AddRange(new byte[0x20]);
-
-//            for (int i = 0; i < value.Length; i++)
-//            {
-//                value[i].offset = newData.Count - 0x20;
-//                newData.AddRange(value[i].ToByteArray(i));
-//                while (newData.Count % 0x20 != 0)
-//                    newData.Add(0);
-//            }
-
-//            int footerOffset = newData.Count;
-
-//            List<GcWavInfo> listForPart2 = new List<GcWavInfo>();
-
-//            for (int i = 0; i < value.Length; i++)
-//            {
-//                newData.AddRange(BitConverter.GetBytes(value[i].offset).Reverse());
-
-//                for (int j = 0; j < value[i].numSamples; j++)
-//                    listForPart2.Add(value[i].soundEntries[j]);
-//            }
-
-//            listForPart2 = listForPart2.OrderBy(f => f._assetID).ToList();
-
-//            foreach (GcWavInfo a in listForPart2)
-//                newData.AddRange(a.PartTwoToByteArray());
-
-//            //newData.AddRange(Data.Skip(EOF));
-
-//            byte unkCount = 0; //Data[0x1F];
-
-//            // hacky solution
-//            AssetSNDI_GCN_V2 asset = new AssetSNDI_GCN_V2(
-//                new Section_AHDR(AHDR.assetID, AHDR.assetType, AHDR.flags, AHDR.ADBG, newData.ToArray()), Game.Incredibles, Endianness.Big)
-//            {
-//                FooterOffset = footerOffset,
-//                assetID = AHDR.assetID,
-//                FileCount = (byte)value.Length,
-//                TotalSoundCount = (short)listForPart2.Count,
-//                SoundCountFirstFile = (short)(value.Length > 0 ? value[0].numSamples : 0),
-//                UnknownCount = unkCount
-//            };
-//            asset.SoundCountRest = (short)(asset.TotalSoundCount - asset.SoundCountFirstFile);
-
-//            return asset.AHDR.data;
-//        }
-
-//        public void Merge(AssetSNDI_GCN_V2 assetSNDI)
-//        {
-//            List<FSB3_File> newEntries = new List<FSB3_File>();
-
-//            FSB3_File first = Entries[0];
-//            first.Merge(assetSNDI.Entries[0]);
-
-//            newEntries.Add(first);
-
-//            for (int i = 1; i < Entries.Length; i++)
-//                newEntries.Add(Entries[i]);
-//            for (int i = 1; i < assetSNDI.Entries.Length; i++)
-//                newEntries.Add(assetSNDI.Entries[i]);
-
-//            Entries = newEntries.ToArray();
-//        }
-
-//        public void AddEntry(byte[] soundData, uint assetID)
-//        {
-//            RemoveEntry(assetID);
-
-//            List<FSB3_File> newEntries = Entries.ToList();
-
-//            if (newEntries.Count == 0)
-//                newEntries.Add(new FSB3_File());
-
-//            FSB3_File temp = new FSB3_File(new BinaryReader(new MemoryStream(soundData)));
-//            temp.soundEntries[0].Sound = assetID;
-//            newEntries[0].Merge(temp);
-//            //newEntries.Add(temp);
-
-//            Entries = newEntries.ToArray();
-//        }
-
-//        public void RemoveEntry(uint assetID)
-//        {
-//            List<FSB3_File> entries = Entries.ToList();
-
-//            for (int i = 0; i < entries.Count; i++)
-//            {
-//                List<GcWavInfo> soundEntries = entries[i].soundEntries.ToList();
-
-//                for (int j = 0; j < soundEntries.Count; j++)
-//                    if (soundEntries[j].Sound == assetID)
-//                    {
-//                        soundEntries.RemoveAt(j);
-//                        j--;
-//                    }
-
-//                entries[i].soundEntries = soundEntries.ToArray();
-
-//                if (entries[i].numSamples == 0)
-//                {
-//                    entries.RemoveAt(i);
-//                    i--;
-//                }
-//            }
-
-//            Entries = entries.ToArray();
-//        }
-
-//        public byte[] GetHeader(uint assetID)
-//        {
-//            foreach (FSB3_File f in Entries)
-//                for (int i = 0; i < f.numSamples; i++)
-//                    if (f.soundEntries[i].Sound == assetID)
-//                    {
-//                        var r = f;
-//                        r.soundEntries = new GcWavInfo[] { f.soundEntries[i] };
-//                        return SerializeData(r, AHDR).Skip(0x20).ToArray();
-//                    }
-
-//            throw new Exception($"Error: SNDI asset does not contain sound header for asset [{assetID:X8}]");
-//        }
-
-//        private byte[] SerializeData(FSB3_File r, Section_AHDR AHDR) => SerializeData(new FSB3_File[] { r }, AHDR);
-
-//        public void Clean(IEnumerable<uint> assetIDs)
-//        {
-//            var fsb3s = Entries.ToList();
-
-//            for (int i = 0; i < fsb3s.Count; i++)
-//            {
-//                var entries = fsb3s[i].soundEntries.ToList();
-//                for (int j = 0; i < entries.Count; j++)
-//                    if (!assetIDs.Contains(entries[j].Sound))
-//                        entries.RemoveAt(j--);
-//                if (entries.Count == 0)
-//                    fsb3s.RemoveAt(i--);
-//                else
-//                    fsb3s[i].soundEntries = entries.ToArray();
-//            }
-
-//            Entries = fsb3s.ToArray();
-//        }
-//    }
-//}
+﻿using HipHopFile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IndustrialPark
+{
+    public class AssetSNDI_GCN_V2 : Asset
+    {
+        public override string AssetInfo => $"GameCube {game}, {Entries.Sum(e => e.soundEntries.Length)} entries";
+
+        public int pFMusicMod { get; set; }
+        public int pFSBFileArray { get; set; }
+        public int pWavInfoArray { get; set; }
+        public int pCutsceneAudioHeaders { get; set; }
+        public ushort nWavFiles { get; set; }
+        public ushort nSounds { get; set; }
+        public ushort nStreams { get; set; }
+        public byte nFSBFiles { get; set; }
+        public byte nCutsceneAudioHeaders { get; set; }
+        public FSB3_File[] Entries { get; set; }
+        public EntrySoundInfo_GCN_V1[] Entries_Sound_CIN { get; set; }
+
+        public AssetSNDI_GCN_V2(string assetName) : base(assetName, AssetType.SoundInfo)
+        {
+            Entries = new FSB3_File[0];
+            Entries_Sound_CIN = new EntrySoundInfo_GCN_V1[0];
+        }
+
+        public AssetSNDI_GCN_V2(Section_AHDR AHDR, Game game, Endianness endianness) : base(AHDR, game)
+        {
+            using (var reader = new EndianBinaryReader(AHDR.data, Endianness.Big))
+            {
+                reader.ReadUInt32();
+                var totalSize = reader.ReadUInt32();
+                pFMusicMod = reader.ReadInt32();
+                pFSBFileArray = reader.ReadInt32();
+                pWavInfoArray = reader.ReadInt32();
+                pCutsceneAudioHeaders = reader.ReadInt32();
+                nWavFiles = reader.ReadUInt16();
+                nSounds = reader.ReadUInt16();
+                nStreams = reader.ReadUInt16();
+                nFSBFiles = reader.ReadByte();
+                nCutsceneAudioHeaders = reader.ReadByte();
+
+                reader.BaseStream.Position = totalSize + 0x20;
+
+                List<FSB3_File> entries = new List<FSB3_File>();
+                for (int i = 0; i < nFSBFiles; i++)
+                    using (var reader2 = new EndianBinaryReader(AHDR.data, Endianness.Little))
+                    {
+                        reader2.BaseStream.Position = reader.ReadUInt32() + 0x20;
+                        entries.Add(new FSB3_File(reader2));
+                    }
+
+                for (int i = 0; i < nWavFiles; i++)
+                {
+                    var assetID = reader.ReadUInt32();
+                    var uFlags = reader.ReadByte();
+                    var uAudioSampleIndex = reader.ReadByte();
+                    var uFSBIndex = reader.ReadByte();
+                    var uSoundInfoIndex = reader.ReadByte();
+                    entries[uFSBIndex].soundEntries[uAudioSampleIndex].SetEntryPartTwo(assetID, uFlags, uAudioSampleIndex, uFSBIndex, uSoundInfoIndex);
+                }
+                Entries = entries.ToArray();
+
+                Entries_Sound_CIN = new EntrySoundInfo_GCN_V1[nCutsceneAudioHeaders];
+                for (int i = 0; i < Entries_Sound_CIN.Length; i++)
+                    Entries_Sound_CIN[i] = new EntrySoundInfo_GCN_V1(reader);
+            }
+        }
+
+        public override void Serialize(EndianBinaryWriter writer)
+        {
+            writer.BaseStream.Position = 0x20;
+            for (int i = 0; i < Entries.Length; i++)
+            {
+                Entries[i].offset = (int)(writer.BaseStream.Position - 0x20);
+                writer.Write(Entries[i].ToByteArray(i));
+                while (writer.BaseStream.Position % 0x20 != 0)
+                    writer.BaseStream.Position++;
+                writer.BaseStream.Position -= 0x04;
+            }
+
+            int footerOffset = (int)(writer.BaseStream.Position - 0x20);
+            List<GcWavInfo> listForPart2 = new List<GcWavInfo>();
+
+            for (int i = 0; i < Entries.Length; i++)
+            {
+                writer.Write(Entries[i].offset);
+                for (int j = 0; j < Entries[i].numSamples; j++)
+                {
+                    Entries[i].soundEntries[j].uAudioSampleIndex = (byte)j;
+                    Entries[i].soundEntries[j].uFSBIndex = (byte)i;
+                    listForPart2.Add(Entries[i].soundEntries[j]);
+                }
+            }
+
+            listForPart2 = listForPart2.OrderBy(f => f._assetID).ToList();
+            foreach (GcWavInfo entry in listForPart2)
+                entry.PartTwoToByteArray(writer);
+
+            foreach (var entry in Entries_Sound_CIN)
+                entry.Serialize(writer);
+
+            writer.BaseStream.Position = 0;
+            writer.Write(assetID);
+            writer.Write(footerOffset);
+            writer.Write(pFMusicMod);
+            writer.Write(pFSBFileArray);
+            writer.Write(pWavInfoArray);
+            writer.Write(pCutsceneAudioHeaders);
+            writer.Write(nWavFiles);
+            writer.Write(nSounds);
+            writer.Write(nStreams);
+            writer.Write(nFSBFiles);
+            writer.Write(nCutsceneAudioHeaders);
+        }
+
+        public byte[] GetHeader(uint assetID)
+        {
+            foreach (FSB3_File f in Entries)
+                for (int i = 0; i < f.numSamples; i++)
+                    if (f.soundEntries[i].Sound == assetID)
+                        return f.SerializeSingleEntry(i);
+
+            throw new Exception($"Error: SNDI asset does not contain sound header for asset [{assetID:X8}]");
+        }
+
+        public void AddEntry(byte[] soundData, uint assetID)
+        {
+            RemoveEntry(assetID);
+
+            using (var reader2 = new EndianBinaryReader(soundData, Endianness.Little))
+            {
+                FSB3_File temp = new FSB3_File(reader2, true);
+                List<FSB3_File> newEntries = Entries.ToList();
+
+                if (temp.soundEntries[0].uFlags == 2)
+                {
+                    newEntries.Add(temp);
+                }
+                else
+                {
+                    if (newEntries.Count == 0)
+                        newEntries.Add(new FSB3_File());
+                    temp.soundEntries[0].Sound = assetID;
+                    newEntries[0].Merge(temp);
+                }
+
+                Entries = newEntries.ToArray();
+            }
+        }
+
+        public void Merge(AssetSNDI_GCN_V2 assetSNDI)
+        {
+            List<FSB3_File> newEntries = new List<FSB3_File>();
+
+            FSB3_File first = Entries[0];
+            first.Merge(assetSNDI.Entries[0]);
+
+            newEntries.Add(first);
+
+            for (int i = 1; i < Entries.Length; i++)
+                newEntries.Add(Entries[i]);
+            for (int i = 1; i < assetSNDI.Entries.Length; i++)
+                newEntries.Add(assetSNDI.Entries[i]);
+
+            Entries = newEntries.ToArray();
+        }
+
+        public void RemoveEntry(uint assetID)
+        {
+            List<FSB3_File> entries = Entries.ToList();
+
+            for (int i = 0; i < entries.Count; i++)
+            {
+                List<GcWavInfo> soundEntries = entries[i].soundEntries.ToList();
+
+                for (int j = 0; j < soundEntries.Count; j++)
+                    if (soundEntries[j].Sound == assetID)
+                        soundEntries.RemoveAt(j--);
+
+                entries[i].soundEntries = soundEntries.ToArray();
+
+                if (entries[i].numSamples == 0)
+                    entries.RemoveAt(i--);
+            }
+
+            Entries = entries.ToArray();
+        }
+
+        public void Clean(IEnumerable<uint> assetIDs)
+        {
+            var fsb3s = Entries.ToList();
+
+            for (int i = 0; i < fsb3s.Count; i++)
+            {
+                var entries = fsb3s[i].soundEntries.ToList();
+                for (int j = 0; i < entries.Count; j++)
+                    if (!assetIDs.Contains(entries[j].Sound))
+                        entries.RemoveAt(j--);
+                if (entries.Count == 0)
+                    fsb3s.RemoveAt(i--);
+                else
+                    fsb3s[i].soundEntries = entries.ToArray();
+            }
+
+            Entries = fsb3s.ToArray();
+        }
+    }
+}
